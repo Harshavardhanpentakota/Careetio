9bbd7fa6f46bb743e46b2e2571d9abf18877f03f
Harsha Vardhan Pentakota <harshavardhan.pentakota19@gmail.com>
2024-07-27 15:49:34 +0530

    evt, err - console log

--- Backend/Routes/account.js
+++ Backend/Routes/account.js
@@ -18,6 +18,7 @@ async function (req,res) {
         const wh = new Webhook(process.env.CLERK_SECRET_KEY);
         console.log(wh);
         const evt = wh.verify(payloadString,svixHeaders);
+        console.log(evt);
         const {id, ...attributes} = evt.data;
         const eventType = evt.type;
         console.log('Webhook verified:', evt);
@@ -33,6 +34,7 @@ async function (req,res) {
     }
     catch(err){
         console.log("Webhook received catch")
+        console.log(err);
         res.status(400).json({
             success:false,
             message:err.message
